92f26f4343543dd4fc38d841c658daca42227aac
Qujohujo <quinnjpete@gmail.com>
2021-02-01 13:44:28 -0700

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Assignment3.Models;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace Assignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(ApplicationResponse appResponse)
+        {
+            if (appResponse.title == "Independence Day")
+            {
+                ModelState.AddModelError(string.Empty, "That is an invalid movie title.");
+                return View(/*"Movies"*/);
+            }
+            else if (ModelState.IsValid)
+            {
+                TempStorage.AddMovie(appResponse);
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
+        public IActionResult Movies()
+        {
+            return View(TempStorage.MovieAdditions);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ApplicationResponse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Models
+{
+    public class ApplicationResponse
+    {
+        [Required]
+        public string category { get; set; }
+
+        [Required]
+        //[RegularExpression("Independence Day", ErrorMessage = "That movie cannot be entered.")]
+        public string title { get; set; }
+
+        [Required]
+        public int year { get; set; }
+
+        [Required]
+        public string director { get; set; }
+
+        [Required]
+        public string rating { get; set; }
+
+        public string edited { get; set; }
+
+        public string lent { get; set; }
+
+        [StringLength(25)]
+        public string notes { get; set; }
+    }
+}
